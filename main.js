3846b3e87d16d6442963c7d86a74d1e78c8df4cf
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-04-24 23:53:39 +0100

    last upgrades and add hamburguer menu

---
+++ main.js
@@ -0,0 +1,11 @@
+function clicarhamburguer(){
+    var abrirmenu=document.getElementsByClassName("navmobile")[0]
+    if (abrirmenu.style.display === "block") {
+        abrirmenu.style.display = "none";
+      } else {
+        abrirmenu.style.display = "block";
+      }
+}
+
+
+
